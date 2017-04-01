eb7edbfd59d08b9b6f6aeb1a03a07d548406bbfd
rivermanvr <vrgithub@riversconsulting.com>
2017-04-01 13:27:45 -0400

    created initial set of folders and files

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const path = require( 'path' );
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    module: {
+        loaders: [{
+            test: /\.js$/,
+            loader: 'babel-loader',
+            exclude: /node_modules/,
+            query: {
+                presets: ['es2015', 'react']
+            }
+        }]
+    }
+};
+
