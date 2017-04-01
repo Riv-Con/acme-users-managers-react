d65cb60f50b7332fcd4fb75e93175bd356f7a2b1
rivermanvr <vrgithub@riversconsulting.com>
2017-04-01 15:58:59 -0400

    index.html set-up

---
+++ route/index.js
@@ -0,0 +1,8 @@
+const express = require( 'express' );
+const router = express.Router();
+const db = require( '../db' );
+const models = db.models;
+
+//need routes in here.....
+
+module.exports = router;
