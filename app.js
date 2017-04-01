6d8f9f7af7761f17481a7128647234143478d002
rivermanvr <vrgithub@riversconsulting.com>
2017-04-01 14:52:14 -0400

    app.js completed

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require( 'express' );
+const app = express();
+
+const path = require( 'path' );
+const routes = require( './routes' );
+const bodyParser = require( 'body-parser' );
+
+app.use('/vendor', express.static(path.join(__dirname, 'node_modules')));
+app.use('/dist', express.static(path.join(__dirname, 'dist')));
+app.use('/css', express.static(path.join(__dirname, 'css')));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use('/api', routes);
+
+app.get('/', (req, res, next) => {
+    res.sendFile(path.join(__dirname, 'views/index.html'));
+});
+
+module.exports = app;
