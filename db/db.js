10f2070278c7e3472620e16b45b02a6f9c2bc9b4
rivermanvr <vrgithub@riversconsulting.com>
2017-04-01 16:23:08 -0400

    fixed typos in db files

--- db/db.js
+++ db/db.js
@@ -1,3 +1,5 @@
 const Sequelize = require( 'sequelize' );
-const connectDB = process.env.DATABASE_URL || 'postgree://localhost/acme_sql';
+const connectDB = process.env.DATABASE_URL || 'postgres://localhost/acme_sql';
 const db = new Sequelize(connectDB, { logging: false });
+
+module.exports = db;
