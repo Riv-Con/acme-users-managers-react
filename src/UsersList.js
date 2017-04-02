e85253e9ee03f2069db9e7d64d20751d3293363b
rivermanvr <vrgithub@riversconsulting.com>
2017-04-02 00:54:37 -0400

    user route completed

--- src/UsersList.js
+++ src/UsersList.js
@@ -7,23 +7,26 @@ const UsersList = ({ users }) => {
         <div className="row">
             <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12">
                 <h5 className="bodyHeading">Users (with Manager):</h5>
-                <div className="panel panel-default">
-                    { users.map( user =>
-                        (<li className="list-group-item" key={ user.id }>
-                            <div className="panel-heading">
-                                { user.name }
+                { users.map( user =>
+                    (<div className="panel panel-default" key={ user.id }>
+                        <div key={ user.id }>
+                            <div>
+                                <div className="panel-heading">
+                                    { user.name }
+                                </div>
+                                <div className="panel-body">
+                                    Managed by: { '  ' }
+                                    { user.managerId ? 
+                                        (<NavLink to="/users/edit">{ user.managerId }</NavLink>) :
+                                        (<NavLink to="/users/edit">{ 'nobody' }</NavLink>)
+                                    }
+                                </div>
                             </div>
-                            <div className="panel-body">
-                                Managed by: 
-                                { user.managerId ? 
-                                    (<NavLink to="/users/edit">{ user.managerId }</NavLink>) :
-                                    (<NavLink to="/users/edit">{ 'nobody' }</NavLink>)
-                                }
-                            </div>
-                        </li>
-                        ))
-                    }
-                </div>
+                        </div>
+                    </div>
+                    ))
+                }
+                Note: Click the manager name to assign a manager.
             </div>
         </div>
     )
