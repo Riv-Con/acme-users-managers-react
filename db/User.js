724a314522aa7a2eb3d37aca5e07db525d897014
rivermanvr <vrgithub@riversconsulting.com>
2017-04-01 20:26:02 -0400

    methods in User.js added...

--- db/User.js
+++ db/User.js
@@ -4,6 +4,10 @@ const defineAttr = {
     name: {
         type: acmeDB.Sequelize.STRING,
         allowNull: false
+    },
+    isMgr: {
+        type: acmeDB.Sequelize.BOOLEAN,
+        defaultValue: false
     }
 };
 
@@ -18,6 +22,66 @@ const defineMethods = {
         }
     }
 };
+
+// const defineMethods = {
+//     classMethods: {
+//         managerRecords: function() {
+//             return this.findAll({ 
+//                 where: {isMgr: true },
+//                 include: [
+//                     { model: this,
+//                         as: 'teamMember'
+//                     }
+//                 ],
+//                 order: [
+//                     ['name', 'ASC']
+//                 ]
+//             })
+//         },
+//         teamRecords: function() {
+//             return this.findAll({
+//                 order: [
+//                     ['name', 'ASC']
+//                 ]
+//             })
+//         },
+//         findRemMgrId: function (selectedId) {
+//             return this.findAll({
+//                 where: {managerId: selectedId}
+//             })
+//             .then(_users => {
+//                 _users.forEach(_user => {
+//                     _user.managerId = null;
+//                     _user.save();
+//                 })
+//             })
+//         },
+//         findById: function (selectedId) {
+//             return this.findOne({
+//                 where: {id: selectedId}
+//             })
+//         },
+//         findUpdateMgrById: function (selectedId) {
+//             let mgrId;
+//             return this.findById(selectedId)
+//                 .then(_mgr => {
+//                     mgrId = _mgr.id;
+//                     _mgr.isMgr = !_mgr.isMgr;
+//                     return _mgr.save();
+//                 })
+//                 .then(() => {
+//                     return this.findRemMgrId(mgrId)
+//                 })
+//         },
+//         assignMgr: function(selectedId, mgrId) {
+//             return this.findById(selectedId)
+//                 .then(_mgr => {
+//                     _mgr.managerId = mgrId;
+//                     return _mgr.save();
+//                 })
+//         }
+//     }
+// };
 
 const User = acmeDB.define('user', defineAttr, defineMethods);
 
